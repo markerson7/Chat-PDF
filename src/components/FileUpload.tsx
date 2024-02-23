8635774f9686350fd50ea5550ac30f7162981394
markerson7 <markersondflomo@gmil.com>
2024-02-23 11:01:45 +0200

    chat with pdf

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { uploadToS3 } from '@/lib/s3'
+import { useMutation } from '@tanstack/react-query'
+import { Inbox, Loader2 } from 'lucide-react'
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+
+const FileUpload = () => {
+    const [uploading, setUploading] = React.useState(false)
+
+    const {mutate, isPending} = useMutation({
+        mutationFn: async ({
+            file_key, 
+            file_name}: {
+            file_key:string;
+            file_name:string;
+            }) =>{
+            const response = await axios.post('/api/create-chat', {file_key, file_name})
+            return response.data;
+        }
+    })
+
+    const {getRootProps, getInputProps} = useDropzone({
+        accept:{'application/pdf': ['.pdf'] },
+        maxFiles:1,
+        onDrop: async (acceptedFiles)=>{           
+            const file = acceptedFiles[0]
+
+            if(file.size > 30 * 1024 * 1024){
+                //bigger then 30mb!
+                toast.error("File too Large!");
+                return
+            }
+
+            try {
+                setUploading(true)
+                const data = await uploadToS3(file)
+                if(!data?.file_key || !data.file_name){
+                    toast.error("something went wrong!");
+                    return;
+                }
+                mutate(data, {
+                    onSuccess: (data) =>{
+                        console.log(data);
+                        //toast.success(data.message);
+                    },
+                    onError: (err) => {
+                        toast.error("Error creating chat!");
+                    }
+                })
+                console.log('data',data)
+            } catch (error) {
+                console.log(error)
+            } finally{
+                setUploading(false)
+            }
+        }
+    })
+  return (
+    <div className='p-2 bg-white rounded-xl'> 
+    <div {...getRootProps({
+        className:'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col'
+    })}>
+        <input {...getInputProps()}/>
+        {(uploading || isPending) ?(
+        <>
+        {/* loading state */}
+        <Loader2 className='h=10 w=10 text-blue-500 animate-spin'/>
+        <p className='mt-2 text-sm text-slate-400'>
+            Loading to GPT.....
+        </p>
+        </>
+        ):(
+            <>
+            <Inbox className='w-10 h-10 text-blue-500'/>
+            <p className='mt-2 text-sm text-slate-400'>Drop PDF Here!</p>
+            </>
+        )}
+        
+    </div>
+    </div>
+  )
+}
+
+export default FileUpload
